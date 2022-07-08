ced83b44793c13438825fd5e40da2ef0fd78b7c8
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-07-08 07:49:17 +0000

    Fix typo/mistake

--- pages/portfolio/illust/[url].tsx
+++ pages/portfolio/illust/[url].tsx
@@ -18,12 +18,12 @@ export default function Illust() {
   return (
     <>
       <div className="bg-base-200 min-h-screen">
-        <span className="text-gray-900">
+        <p className="text-gray-900">
           <Title title={data.title} />
-        </span>
-        <button className="pt-2 text-md flex justify-center text-gray-700">
+        </p>
+        <p className="pt-2 text-md flex justify-center text-gray-700">
           {data.description}
-        </button>
+        </p>
         <button
           className="btn btn-ghost fixed top-1 right-1 sm:top-5 sm:right-5 text-gray-600/[.60] text-5xl z-50"
           onClick={() => router.push("/portfolio/illust/")}
