f64f07aa82ed7c675111b606c5459db63624240d
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-07-13 01:58:47 +0900

    reupload

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,9 +1,27 @@
 import Title from "../../components/title";
+import { useRouter } from "next/router" 
 export default function Blog() {
+  const router = useRouter()
   return (
     <>
       <Title title="Blog" />
-      <p>まだ書いていない。正しくはめんどい。</p>
+      <div
+          className="card w-96 bg-base-100 shadow-xl shadow-lg hover:shadow-md hover:brightness-[.98]"
+          onClick={() => router.push("/blog/2022-07-13")}
+        >
+          <div className="card-body">
+            <h2 className="card-title">2022-07-13</h2>
+            <p>これテスト投稿なんで見る価値ないです</p>
+            <div className="card-actions justify-end">
+              <button
+                className="btn btn-primary"
+                onClick={() => router.push("/blog/2022-07-13")}
+              >
+                Go
+              </button>
+            </div>
+          </div>
+        </div>
     </>
   );
 }
