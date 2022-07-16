dd75f18ef14ac25f47e81aa3bbbb205e6a2c21ef
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-07-16 23:59:51 +0900

    Add NextNProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
 import { useRouter } from "next/router";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -10,6 +11,13 @@ function MyApp({ Component, pageProps }) {
   usebar = router.pathname !== "/portfolio/images/[url]";
   return (
     <>
+      <NextNProgress
+        startPosition={0.45}
+        stopDelayMs={120}
+        height={4}
+        showOnShallow={true}
+        options={{ showSpinner: true, }}
+      />
       {usebar ? <Navbar /> : null}
       <div className={usebar ? "pt-[64px] min-h-screen" : null}>
         <Component {...pageProps} />
